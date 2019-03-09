2d92c2867075609e0347d46d658d5129cd1e7c58
svensone <svenson.ertel@gmail.com>
2019-03-09 17:58:41 +0800

    update link href

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -32,7 +32,7 @@ class AppNavbar extends Component {
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                  <NavLink href="https://github.com/bradtraversy/mern_shopping_list">
+                  <NavLink href="https://github.com/Svensone/MERN-Stack-Shopping-List">
                     Github Repo
                   </NavLink>
                 </NavItem>
